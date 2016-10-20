3ab7bbfd52a8e45ead4670bdef607129facaf56f
zlyu88 <zlyu88@gmail.com>
2016-10-20 17:35:34 +0300

    Changing models.py according to comments

--- serenity/user_info/models.py
+++ serenity/user_info/models.py
@@ -1,23 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-#
-#
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-#
-#
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
 
 
 class User_info(models.Model):
     name = models.CharField(max_length=50)
-    second_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField(validators=[MinValueValidator(10)])
     sex = models.CharField(max_length=50)
-    picture = models.ImageField(max_length=50)
+    picture = models.ImageField(upload_to='avatars', height_field=50, width_field=50)
